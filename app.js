191c7339fdcd82c0c9eabe33acd0da078eba99d1
Srikant Sarda <ssarda@Srikants-MacBook-Air.local>
2015-09-22 18:32:24 -0400

    First day done

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var swig = require('swig');
 var app = express();
 
 var router = express.Router();
+
+app.engine('html', swig.renderFile);
+app.set('view engine','html');
+app.set('views',__dirname + '/views');
+swig.setDefaults({cache: false});
 
 router.use(function(req, res, next) {
 	console.log(req.method + ' ' + req.url);
@@ -9,7 +15,14 @@ router.use(function(req, res, next) {
 })
 
 router.get('/', function(req, res) {
-	res.send('Hello World');
+	//res.send('hello world');
+	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
+	// res.send(swig.renderFile('./views/index.html', {title: "An Example", 
+	// 		people: {P1: {name: "Gandalf"}, 
+	// 				 P2: {name: "Frodo"}, 
+	// 				 P3: {name: "Hermione"} }
+	// 		}));
 })
 
 router.get('/news', function(req, res) {
