f305cada0579022ff1023c69a1abff05c0a611dd
Srikant Sarda <ssarda@Srikants-MacBook-Air.local>
2015-09-22 17:37:28 -0400

    Starting collaboration

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ var app = express();
 
 var router = express.Router();
 
+router.use(function(req, res, next) {
+	console.log(req.method + ' ' + req.url + ' ' + res.statusCode);
+	next();
+})
+
 router.get('/', function(req, res) {
 	res.send('Hello World');
 })
+
+router.get('/news', function(req, res) {
+	res.send('this is the news!');
+})
+
 
 app.use(router);
 
