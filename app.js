89539637138a80963595ba4d0550317a3169e208
Srikant Sarda <ssarda@Srikants-MacBook-Air.local>
2015-09-22 17:18:08 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+	res.send('Hello World');
+})
+
+app.use(router);
+
+app.listen(3000, function() {
+	console.log("listening..");
+})
